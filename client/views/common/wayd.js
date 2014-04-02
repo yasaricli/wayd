da2a6a2beddd711a96f37f4bbd8e969af2649be7
Yasar icli <yasaricli@gmail.com>
2014-04-02 19:01:41 +0300

    bird animation

--- client/views/common/wayd.js
+++ client/views/common/wayd.js
@@ -1,4 +1,12 @@
 (function() {
-
-
+    Planet("index") ({
+        rendered: function() {
+            // bird show;
+            bird.hey('show');
+        },
+        helpers: {},
+        events: {
+        
+        }
+    });
 }).call(this);
