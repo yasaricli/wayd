c9ca2961311fd7fe96bd656ffae790017e76b1a9
Yasar icli <yasaricli@gmail.com>
2014-04-03 08:05:53 -0400

    methods server directory and client/wayd common js updated.

--- server/methods/emails.js
+++ server/methods/emails.js
@@ -1,8 +1,6 @@
-
 Meteor.methods({
-    newEmail: function(val) {
+    saveEmail: function(val) {
         var email = Emails.findOne({ email: val });
-        console.log(email, val);
         if (email) { 
             throw new Meteor.Error(422, 'This email is already subscribed.' );      
         }
